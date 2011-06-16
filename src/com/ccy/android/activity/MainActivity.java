199d316301cae89ae69e12f27f8868884ad30799
chencangyi <chencangyi@gmail.com>
2011-06-16 04:55:36 +0000

    add CardReader module.

--- src/com/ccy/android/activity/MainActivity.java
+++ src/com/ccy/android/activity/MainActivity.java
@@ -1,14 +1,92 @@
 package com.ccy.android.activity;
 
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.friendlyarm.AndroidSDK.HardwareControler;
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+    static final String DEBUG_TAG = "DEBUG";
+    private int port = 3333;
+    private String ServerIP = "58.41.211.124" ;
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+    	
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         HardwareControler.setLedState(0, 1);
+        new CardReaderThread().start();
+        
+    }
+    class CardReaderThread extends Thread
+    {
+    	public void run()
+    	{
+    		int ret = -1;
+    		Socket socket = null;
+    		CardReader.led_control((byte)0x01);
+            CardReader.bell_control();
+            CardReader.power_on_card();
+            while(true)
+            {
+            	try {
+    				Thread.sleep(200);
+    			} catch (InterruptedException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+    	        ret = CardReader.active_card();
+    	        if(ret == 0)
+    	        {
+    	        	String message = CardReader.Rx_Buffer.toString() + "\r\n"; 
+    	        	Log.d(DEBUG_TAG,"message = " + message);
+    	        	Log.d(DEBUG_TAG,"length = " + message.length());
+    	        	try 
+    	    		{	
+    	    			//创建Socket
+    	    			socket = new Socket(ServerIP,port); 
+    	    			//向服务器发送消息
+    	    			PrintWriter out = new PrintWriter( new BufferedWriter( new OutputStreamWriter(socket.getOutputStream())),true);      
+    	    			out.println(message); 
+    	    			
+    	    			//接收来自服务器的消息
+    	    			/*
+    	    			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+    	    			String msg = br.readLine(); 
+    	    			
+    	    			if ( msg != null )
+    	    			{
+    	    				mTextView.setText(msg);
+    	    			}
+    	    			else
+    	    			{
+    	    				mTextView.setText("数据错误!");
+    	    			}*/
+    	    			//关闭流
+    	    			out.close();
+    	    			//br.close();
+    	    			//关闭Socket
+    	    			socket.close(); 
+    	    		}
+    	    		catch (Exception e) 
+    	    		{
+    	    			// TODO: handle exception
+    	    			Log.e(DEBUG_TAG, e.toString());
+    	    		}
+    	    		Intent intent = new Intent();
+    	    		intent.setClass(MainActivity.this, SecondActivity.class);
+    	    		startActivity(intent);
+    	        }
+            }
+    	}
     }
 }
